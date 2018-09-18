27734855e2073301ea6aa3c3e4b06040570c6314
Srikanth Bandaru <srikanthb602@gmail.com>
2018-09-17 19:02:45 -0500

    Donate page layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import Donate from '../Donate';
 class App extends Component {
   render() {
     return (
-      <div className="">
+      <div className="container">
         <header className="">
           <h1 className="">Donate Page</h1>
         </header>
