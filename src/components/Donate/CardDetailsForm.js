08834e3caa5c500ac71452228321405a9dcdd7f0
Srikanth Bandaru <srikanthb602@gmail.com>
2018-10-05 14:26:55 -0500

    Use card element instead of multiple stripe elements

--- src/components/Donate/CardDetailsForm.js
+++ src/components/Donate/CardDetailsForm.js
@@ -1,61 +1,67 @@
-import React from 'react';
-import {
-	CardNumberElement,
-	CardExpiryElement,
-	CardCVCElement,
-	PostalCodeElement
-} from 'react-stripe-elements';
+import React from "react";
+import { CardElement } from "react-stripe-elements";
 
-import { donationRef } from '../../config/firebase';
+import { donationRef } from "../../config/firebase";
 
 class CardDetailsForm extends React.Component {
-	handleSubmit = event => {
-		event.preventDefault();
-		const { donateForm } = this.props;
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-		this.props.stripe.createToken({ name: donateForm.fullName }).then(({ token }) => {
-			donateForm.token = token;
+    handleSubmit = event => {
+        event.preventDefault();
+        const { donateForm } = this.props;
 
-			fetch('http://localhost:8000/api/createDonator', {
-				method: 'post',
-				contentType: 'application/json',
-				body: JSON.stringify(donateForm)
-			}).then((response) => {
-				console.log(response);
-				return response.json();
-			})
+        this.props.stripe.createToken({ name: donateForm.fullName }).then(({ token }) => {
+            donateForm.token = token;
 
-			console.log('Received Stripe token:', donateForm);
+            fetch("http://localhost:8000/api/createDonator", {
+                method: "post",
+                contentType: "application/json",
+                body: JSON.stringify(donateForm)
+            }).then(response => {
+                console.log(response);
+                return response.json();
+            });
 
-			// donationRef.push().set(donateForm);
-		});
-	};
+            console.log("Received Stripe token:", donateForm);
 
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					Card number
-					<CardNumberElement />
-				</label>
-				<div className="row">
-					<div className="col-md-6">
-						<label>
-							Expiration date
-							<CardExpiryElement />
-						</label>
-					</div>
-					<div className="col-md-6">
-						<label>
-							CVC
-							<CardCVCElement />
-						</label>
-					</div>
-				</div>
-				<button>Pay</button>
-			</form>
-		);
-	}
+            // donationRef.push().set(donateForm);
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" className="w-100" value={this.props.donateForm.fullName} />
+                <div className="row">
+                    <div className="col-md-6">
+                        <input
+                            type="email"
+                            placeHolder="Email"
+                            className="w-100"
+                            value={this.state.email}
+                            name="email"
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <input
+                            type="number"
+                            placeholder="Phone"
+                            className="w-100"
+                            value={this.state.phoneNumber}
+                            name="phoneNumber"
+                        />
+                    </div>
+                </div>
+
+                <hr />
+                <CardElement />
+                <button>Pay {this.props.donateForm.donationAmount}</button>
+            </form>
+        );
+    }
 }
 
 export default CardDetailsForm;
