bbeda1643518d893d6a4bd6f3148828ce15c76ae
Srikanth Bandaru <srikanthb602@gmail.com>
2018-10-06 21:01:05 -0500

    Updated format script and prettify

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require("dotenv").config({ path: "./.env.local" });
+require('dotenv').config({ path: './.env.local' });
 
-const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_APIKEY);
-const express = require("express");
-const path = require("path");
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_APIKEY);
+const express = require('express');
+const path = require('path');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8000;
 
@@ -12,56 +12,57 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.text({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
 // API calls
-app.post("/api/createDonator", async (req, res) => {
-    const request = JSON.parse(req.body);
+app.post('/api/createDonator', async (req, res) => {
+	const request = JSON.parse(req.body);
 
-    var customer = await stripe.customers
-        .create({
-            email: request.email,
-            source: request.token.id
-        })
-        .then(customer => {
-            console.log(customer);
+	var customer = await stripe.customers
+		.create({
+			email: request.email,
+			source: request.token.id
+		})
+		.then(customer => {
+			console.log(customer);
 
-            request.customer = customer;
-            if (request.donationFrequency === "oneTime") {
-                return stripe.charges.create({
-                    amount: 5500,
-                    currency: "usd",
-                    description: "Donation",
-                    // source: request.token.id,
-                    customer: customer.id,
-                    statement_descriptor: "Custom descriptor"
-                });
-            }
-        })
-        .then(charges => {
-            console.log(charges);
-        });
+			request.customer = customer;
+			if (request.donationFrequency === 'oneTime') {
+				return stripe.charges.create({
+					amount: 5500,
+					currency: 'usd',
+					description: 'Donation',
+					// source: request.token.id,
+					customer: customer.id,
+					statement_descriptor: 'Custom descriptor'
+				});
+			}
+		})
+		.then(charges => {
+			request.charges = charges;
+			console.log(charges);
+		});
 
-    // create customer
-    // if one time, create charge
-    // else
-    //
+	// create customer
+	// if one time, create charge
+	// else
+	//
 
-    console.log(customer);
+	console.log(customer);
 
-    res.send(req.body);
+	res.send(req.body);
 });
 
-if (process.env.NODE_ENV === "production") {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, "build")));
-    // Handle React routing, return all requests to React app
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "build", "index.html"));
-    });
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'build')));
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/components/Donate/CheckoutForm.js
+++ src/components/Donate/CheckoutForm.js
@@ -1,194 +1,194 @@
-import React from "react";
-import { injectStripe } from "react-stripe-elements";
+import React from 'react';
+import { injectStripe } from 'react-stripe-elements';
 
-import Modal from "../shared/Modal";
-import CardDetailsForm from "./CardDetailsForm";
+import Modal from '../shared/Modal';
+import CardDetailsForm from './CardDetailsForm';
 
 class CheckoutForm extends React.Component {
-    state = {
-        shouldShowStripeCheckoutModal: false,
-        donateForm: {
-            donationFrequency: "monthly",
-            donationAmount: "$10",
-            fullName: "",
-            honoreeName: "",
-            honoreeEmail: "",
-            email: "",
-            phone: ""
-        }
-    };
+	state = {
+		shouldShowStripeCheckoutModal: false,
+		donateForm: {
+			donationFrequency: 'monthly',
+			donationAmount: '$10',
+			fullName: '',
+			honoreeName: '',
+			honoreeEmail: '',
+			email: '',
+			phone: ''
+		}
+	};
 
-    toggleStripeCheckoutModal = event => {
+	toggleStripeCheckoutModal = event => {
 		event.preventDefault();
 		if (this.state.donateForm.fullName.length > 3) {
 			this.setState({
 				shouldShowStripeCheckoutModal: !this.state.shouldShowStripeCheckoutModal
 			});
 		}
-    };
+	};
 
-    handleInputChange = event => {
-        const { donateForm } = this.state;
+	handleInputChange = event => {
+		const { donateForm } = this.state;
 
-        if (!event.target.name) {
-            const name = event.target.querySelector("input").name;
-            const value = event.target.querySelector("input").value;
+		if (!event.target.name) {
+			const name = event.target.querySelector('input').name;
+			const value = event.target.querySelector('input').value;
 
-            donateForm[name] = value;
-        } else {
-            donateForm[event.target.name] = event.target.value;
-        }
+			donateForm[name] = value;
+		} else {
+			donateForm[event.target.name] = event.target.value;
+		}
 
-        this.setState({
-            donateForm
-        });
-    };
+		this.setState({
+			donateForm
+		});
+	};
 
-    render() {
-        const donationChoice = ["$10", "$35", "$50", "$100", "$250"];
-        const renderDonationChoice = amount => (
-            <li
-                className={`choice-amount ${this.state.donateForm.donationAmount === amount ? "selected" : ""}`}
-                style={{
-                    width: `${100 / (donationChoice.length + 1)}%`,
-                    minWidth: `${100 / (donationChoice.length + 1)}%`
-                }}
-                onClick={this.handleInputChange}
-            >
-                <label>
-                    {amount}
-                    <input
-                        className="hidden-radio"
-                        type="radio"
-                        name="donationAmount"
-                        id="donationAmount1"
-                        value={amount}
-                        checked={this.state.donateForm.donationAmount === amount}
-                    />
-                </label>
-            </li>
-        );
-        return (
-            <React.Fragment>
-                {this.state.shouldShowStripeCheckoutModal && (
-                    <Modal
-                        modalBody={
-                            <CardDetailsForm
-                                donateForm={this.state.donateForm}
-                                stripe={this.props.stripe}
-                                handleInputChange={this.handleInputChange}
-                            />
-                        }
-                        subTitle="Donation"
-                        handleClose={this.toggleStripeCheckoutModal}
-                    />
-                )}
+	render() {
+		const donationChoice = ['$10', '$35', '$50', '$100', '$250'];
+		const renderDonationChoice = amount => (
+			<li
+				className={`choice-amount ${this.state.donateForm.donationAmount === amount ? 'selected' : ''}`}
+				style={{
+					width: `${100 / (donationChoice.length + 1)}%`,
+					minWidth: `${100 / (donationChoice.length + 1)}%`
+				}}
+				onClick={this.handleInputChange}
+			>
+				<label>
+					{amount}
+					<input
+						className="hidden-radio"
+						type="radio"
+						name="donationAmount"
+						id="donationAmount1"
+						value={amount}
+						checked={this.state.donateForm.donationAmount === amount}
+					/>
+				</label>
+			</li>
+		);
+		return (
+			<React.Fragment>
+				{this.state.shouldShowStripeCheckoutModal && (
+					<Modal
+						modalBody={
+							<CardDetailsForm
+								donateForm={this.state.donateForm}
+								stripe={this.props.stripe}
+								handleInputChange={this.handleInputChange}
+							/>
+						}
+						subTitle="Donation"
+						handleClose={this.toggleStripeCheckoutModal}
+					/>
+				)}
 
-                <form className="mt-1" id="donate-form">
-                    <div className="form-group frequency">
-                        <label className="col-form-label pt-0">Select your support</label>
-                        <div className="form-check" onClick={this.handleInputChange}>
-                            <label htmlFor="gridRadios1">
-                                Monthly
-                                <input
-                                    className="form-check-input"
-                                    type="radio"
-                                    name="donationFrequency"
-                                    id="donationFrequency1"
-                                    value="monthly"
-                                    checked={this.state.donateForm.donationFrequency === "monthly"}
-                                />
-                            </label>
-                        </div>
-                        <div className="form-check" onClick={this.handleInputChange}>
-                            <label htmlFor="gridRadios2">
-                                One time
-                                <input
-                                    className="form-check-input"
-                                    type="radio"
-                                    name="donationFrequency"
-                                    id="donationFrequency2"
-                                    value="oneTime"
-                                    checked={this.state.donateForm.donationFrequency === "oneTime"}
-                                />
-                            </label>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fullName">Select your amount</label>
-                        <ul className="horizontal-chooser">
-                            {donationChoice.map(amount => renderDonationChoice(amount))}
-                            <li
-                                className="choice-amount"
-                                onClick={this.handleInputChange}
-                                style={{
-                                    width: `${100 / (donationChoice.length + 1)}%`,
-                                    minWidth: `${100 / (donationChoice.length + 1)}%`
-                                }}
-                            >
-                                <label>
-                                    Other
-                                    <input
-                                        type="number"
-                                        onChange={this.handleInputChange}
-                                        name="donationAmount"
-                                        id="other-amount"
-                                        value={this.state.donateForm.donationAmount}
-                                    />
-                                </label>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="fullName">Your name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="fullName"
-                            value={this.state.donateForm.fullName}
-                            onChange={this.handleInputChange}
-                        />
-                    </div>
-                    <button className="btn btn-link p-0" onClick={this.props.toggleHonoreeFields}>
-                        {!this.props.shouldShowHonoreeFields
-                            ? "Click here to donate in honor of another person"
-                            : "Never mind — this gift isn't in honor of another person"}
-                    </button>
-                    <div className={this.props.honoreeFieldsClassName}>
-                        <div className="honoree-details my-2">
-                            <div className="form-group">
-                                <label htmlFor="honoreeName">Honoree's name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    name="honoreeName"
-                                    value={this.state.donateForm.honoreeName}
-                                    onChange={this.handleInputChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="honoreeEmail">Honoree's email</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    name="honoreeEmail"
-                                    value={this.state.donateForm.honoreeEmail}
-                                    onChange={this.handleInputChange}
-                                />
-                                <small>We will notify your honoree of your gift</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="donate-button-group">
-                        <button className="btn btn-primary donate-using-cc" onClick={this.toggleStripeCheckoutModal}>
-                            Use credit card
-                        </button>
-                        <button className="btn btn-primary">Use Paypal</button>
-                    </div>
-                </form>
-            </React.Fragment>
-        );
-    }
+				<form className="mt-1" id="donate-form">
+					<div className="form-group frequency">
+						<label className="col-form-label pt-0">Select your support</label>
+						<div className="form-check" onClick={this.handleInputChange}>
+							<label htmlFor="gridRadios1">
+								Monthly
+								<input
+									className="form-check-input"
+									type="radio"
+									name="donationFrequency"
+									id="donationFrequency1"
+									value="monthly"
+									checked={this.state.donateForm.donationFrequency === 'monthly'}
+								/>
+							</label>
+						</div>
+						<div className="form-check" onClick={this.handleInputChange}>
+							<label htmlFor="gridRadios2">
+								One time
+								<input
+									className="form-check-input"
+									type="radio"
+									name="donationFrequency"
+									id="donationFrequency2"
+									value="oneTime"
+									checked={this.state.donateForm.donationFrequency === 'oneTime'}
+								/>
+							</label>
+						</div>
+					</div>
+					<div className="form-group">
+						<label htmlFor="fullName">Select your amount</label>
+						<ul className="horizontal-chooser">
+							{donationChoice.map(amount => renderDonationChoice(amount))}
+							<li
+								className="choice-amount"
+								onClick={this.handleInputChange}
+								style={{
+									width: `${100 / (donationChoice.length + 1)}%`,
+									minWidth: `${100 / (donationChoice.length + 1)}%`
+								}}
+							>
+								<label>
+									Other
+									<input
+										type="number"
+										onChange={this.handleInputChange}
+										name="donationAmount"
+										id="other-amount"
+										value={this.state.donateForm.donationAmount}
+									/>
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div className="form-group">
+						<label htmlFor="fullName">Your name</label>
+						<input
+							type="text"
+							className="form-control"
+							name="fullName"
+							value={this.state.donateForm.fullName}
+							onChange={this.handleInputChange}
+						/>
+					</div>
+					<button className="btn btn-link p-0" onClick={this.props.toggleHonoreeFields}>
+						{!this.props.shouldShowHonoreeFields
+							? 'Click here to donate in honor of another person'
+							: "Never mind — this gift isn't in honor of another person"}
+					</button>
+					<div className={this.props.honoreeFieldsClassName}>
+						<div className="honoree-details my-2">
+							<div className="form-group">
+								<label htmlFor="honoreeName">Honoree's name</label>
+								<input
+									type="text"
+									className="form-control"
+									name="honoreeName"
+									value={this.state.donateForm.honoreeName}
+									onChange={this.handleInputChange}
+								/>
+							</div>
+							<div className="form-group">
+								<label htmlFor="honoreeEmail">Honoree's email</label>
+								<input
+									type="email"
+									className="form-control"
+									name="honoreeEmail"
+									value={this.state.donateForm.honoreeEmail}
+									onChange={this.handleInputChange}
+								/>
+								<small>We will notify your honoree of your gift</small>
+							</div>
+						</div>
+					</div>
+					<div className="donate-button-group">
+						<button className="btn btn-primary donate-using-cc" onClick={this.toggleStripeCheckoutModal}>
+							Use credit card
+						</button>
+						<button className="btn btn-primary">Use Paypal</button>
+					</div>
+				</form>
+			</React.Fragment>
+		);
+	}
 }
 
 export default injectStripe(CheckoutForm);
