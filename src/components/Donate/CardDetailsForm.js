88fe1e8149fde64af7d841002a8b9c9b541177a6
Srikanth Bandaru <srikanthb602@gmail.com>
2018-10-28 18:11:16 -0500

    Update error message

--- src/components/Donate/CardDetailsForm.js
+++ src/components/Donate/CardDetailsForm.js
@@ -40,7 +40,7 @@ class CardDetailsForm extends React.Component {
 				})
 				.catch(error => {
 					this.setState({
-						error: 'Something went wrong. Please try again later or contact STLTempleEdu@gmail.com',
+						error: 'Something went wrong. Please try again later.',
 						isLoading: false
 					});
 				});
